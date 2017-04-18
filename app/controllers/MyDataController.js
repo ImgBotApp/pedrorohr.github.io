13a4d0753e24e4aba5dff413d747f3001bd68043
Pedro Guilherme Rohr <pedro.guilherme.rohr@gmail.com>
2017-04-18 01:51:53 -0300

    Removed AngularJS stuff

--- app/controllers/MyDataController.js
+++
@@ -1,9 +0,0 @@
-(function() {
-	var app = angular.module('myPage');
-	app.controller('MyDataController', [function() {
-		this.preferredName = 'Pedro';
-		this.firstName = this.preferredName + ' Guilherme';
-		this.lastName = 'Rohr';
-		this.fullName = this.firstName + ' ' + this.lastName;
-	}]);
-})();
